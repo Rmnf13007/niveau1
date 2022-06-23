092f9a9877bb7887bf3a85d23168836ee4cc1451
Romuald Nicolas-Faure <107117972+Rmnf13007@users.noreply.github.com>
2022-06-23 16:59:42 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+var age = prompt("donnez moi votre age");
+
+
+while (isNaN(age) === true || age!=null) 
+{
+    var age = prompt("donnez moi votre age");
+  }
+  a = alert("Hello! I am an alert box!!");
